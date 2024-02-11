c0927ded8ce4f8b5930e7bfd669964887c65269b
Rafamourasantos <159167638+Rafamourasantos@users.noreply.github.com>
2024-02-11 10:40:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ function verificarChute() {
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
-            exibirTextoNaTela('p', 'O número secreto é menor');
+            exibirTextoNaTela('p', 'Bê o número secreto é menor');
         } else {
-            exibirTextoNaTela('p', 'O número secreto é maior');
+            exibirTextoNaTela('p', 'Bê o número secreto é maior');
         }
         tentativas++;
         limparCampo();
